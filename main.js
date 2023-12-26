78b8796f7a93e769e74733013fa9e66ec91a25c7
PedroMuniz10 <pedro.lau@estudante.ufjf.br>
2023-12-26 12:17:38 -0300

    imprimindo o módulo e a quantidade de macas de joao e maria

---
+++ main.js
@@ -0,0 +1,12 @@
+import { getJoao, getMaria } from "./joaoEMaria.js";
+
+// main.js
+
+console.log ('módulo main');
+
+console.log ('Joao tem ', getJoao(), 'macas!');
+console.log ('Maria tem ', getJoao(), 'macas!');
+
+
+
+
